b6365f870ff6536e6d3aa0b45c859b584dffc7fa
loggerJK <jiwon7258@gmail.com>
2025-09-26 21:16:39 +0900

    .

--- qual_new/to_webp.sh
+++ qual_new/to_webp.sh
@@ -1,6 +1,10 @@
-for file in *.pdf; do
-    # pdf to png
-    magick -density 600 "$file" -quality 100 "${file%.pdf}.png"
-    # png to webp
-    cwebp -q 100 "${file%.pdf}.png" -o "${file%.pdf}.webp"
-done
+file='cfg_comparison_sd3.pdf'
+magick -density 600 "$file" -quality 100 "${file%.pdf}.png"
+cwebp -q 100 "${file%.pdf}.png" -o "${file%.pdf}.webp"
+
+# for file in *.pdf; do
+#     # pdf to png
+#     magick -density 600 "$file" -quality 100 "${file%.pdf}.png"
+#     # png to webp
+#     cwebp -q 100 "${file%.pdf}.png" -o "${file%.pdf}.webp"
+# done
